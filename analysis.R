4096fedda1841cd2b164c1bf836805e30c12609e
Lokeshkonreddy <lokeshreddy.konreddy@wmich.edu>
2025-01-15 10:07:29 -0500

    Initial commit: Added analysis.R

--- analysis.R
+++ analysis.R
@@ -1,10 +1,1 @@
-<<<<<<< HEAD
-# Analysis Script
-data <- c(1, 2, 3, 4, 5)
-print(mean(data))  # Print the mean of the data
-print("lokesh")
-name=c('lokesh')
-print(name)
-=======
-"# Analysis Script" 
->>>>>>> bad532ba6fa804120ff362a040a2bd3d41c7dd23
+#File
